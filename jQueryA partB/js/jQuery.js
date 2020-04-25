cdeef4e019f82b6a1c46bb1f5676cd3ed9fda891
abdera123444 <60236312+abdera123444@users.noreply.github.com>
2020-04-25 18:49:38 -0400

    Add files via upload

---
+++ jQueryA partB/js/jQuery.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+  $(function() {
+    $(".rslides").responsiveSlides();
+  });
+
+$("#secret").hide();
+
+$("#button1").click(function(){
+  $("#secret").show(); });
+
+
+$("p").css("font-size" , "23px" );
+
+$("footer").html("<p>Come over here for an extra tip!</p>");
+
+$("footer").mouseover(function(){
+    $("footer p").html("Spread butter on two pieces of toast and heat in the oven to enjoy with your eggs! ");
+});
+
+$("footer").mouseleave(function() {
+  $("footer p").html("Bon Apetit!");
+});
+
+
+
+
+});
